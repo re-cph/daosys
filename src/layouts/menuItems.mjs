d13fb2c971ba1fb3014048bb86d0a5b5c72f54e5
vesters <v@re-cph.com>
2022-03-22 22:10:29 +0100

    Initial commit (1st draft)

---
+++ src/layouts/menuItems.mjs
@@ -0,0 +1,6 @@
+export default [
+  { href: "/#about", name: "About" },
+  { href: "/#services", name: "Services" },
+  { href: "/#Technology", name: "Technology" },
+  { href: "/#Contact", name: "Contact" },
+];
---
+++ tailwind.config.cjs
@@ -0,0 +1,59 @@
+module.exports = {
+  content: [
+    './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    screens: {
+      sm: "768px",
+      md: "1024px",
+      // lg: "1440px",
+      lg: "1200px",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        // lg: "15px", // Might be good?
+      },
+    },
+    fontFamily: {
+      sans: ['"Space Grotesk"', 'sans-serif']
+    },
+    // fontFamily: {
+    //   body: ["Space Grotesk", "sans-serif"],
+    //   header: ["Space Grotesk", "sans-serif"],
+    // },
+    // fontSize: {
+    //   menu: ["15px", "1.7em"],
+    //   body: ["18px", "1.7em"],
+    //   h1: 
+    //   header: ["24px", "1.7em"],
+    //   title: ["20px", "1.7em"],
+    //   caption: ["12px", "1.7em"],
+    // },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: {
+        DEFAULT: "#ffffff",
+        darker: "#F5F5FB",
+        dark: "#DCDCE1"
+      },
+      black: "#000000",
+      blue: {
+        dark: '#232C3B',
+        DEFAULT: '#3A465A',
+        lighter: '#445064',
+        light: '#A2BBCF',
+      },
+      green: {
+        lighter: '#466064',
+        DEFAULT: '#324C50',
+      }
+    },
+    extend: {
+      fontSize: {
+        splash: ['2.125rem', '3.75rem']
+      },
+    },
+  },
+};
