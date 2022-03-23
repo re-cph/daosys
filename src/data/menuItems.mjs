29f34ab64e514905278a1c621c1d17621d5ca5f6
vesters <v@re-cph.com>
2022-03-23 23:09:03 +0100

    Menu makeover

--- src/data/menuItems.mjs
+++ src/data/menuItems.mjs
@@ -1,6 +1,6 @@
 export default [
   { href: "/#about", name: "About" },
   { href: "/#services", name: "Services" },
-  { href: "/#Technology", name: "Technology" },
-  { href: "/#Contact", name: "Contact" },
+  { href: "/#technology", name: "Technology" },
+  { href: "/#contact", name: "Contact" },
 ];
