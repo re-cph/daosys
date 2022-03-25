3704c4e75b9abdd7ef5a1545791b69a8beaa56b2
vesters <v@re-cph.com>
2022-03-25 13:48:53 +0100

    Updates according to Client/Today/Mehlbye feedback

--- src/data/menuItems.mjs
+++ src/data/menuItems.mjs
@@ -1,6 +1,6 @@
 export default [
   { href: "/#about", name: "About" },
   { href: "/#services", name: "Services" },
-  { href: "/#technology", name: "Technology" },
+  // { href: "/#technology", name: "Technology" },
   { href: "/#contact", name: "Contact" },
 ];
